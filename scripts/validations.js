2fbf7c661f8643b1e9da812d469dc4738e1eb157
Polilla <naway.luisju@gmail.com>
2024-11-25 08:13:55 -0600

    finished homepage styling, user validations that doesn't need interaction with the database, the rest of the project will be developed in antares mostly

--- scripts/validations.js
+++ scripts/validations.js
@@ -1,27 +1,112 @@
+function hideWarning(warningId) {
+    var warning = document.getElementById(warningId);
+
+    warning.style.opacity = 0;
+}
+
+function resetWarning(warningId) {
+    var warning = document.getElementById(warningId);
+
+    warning.innerText = "*";
+}
+
 function validateLogin() {
     var username = document.getElementById("username-input").value;
     var password = document.getElementById("password-input").value;
 
+    var usr_m = document.getElementById("username-m");
+    var pswd_m = document.getElementById("password-m");
+
+    var v = true;
+
+    if(username === '') {
+        usr_m.style.opacity = 1;
+
+        v = false;
+    }
+    
+    if(password === '') {
+        pswd_m.style.opacity = 1;
+
+        v = false;
+    }
+
+    // buscar el ususario, si existe, admitirlo
+
     console.log(username);
     console.log(password);
 
-    return false;
+    return v;
 }
 
 function validateRegister() {
     var username = document.getElementById("username-input").value;
 
     var password = document.getElementById("password-input").value;
-    var passwordConfirm = document.getElementById("password-confirm").value;
+    var password_confirm = document.getElementById("password-confirm").value;
 
-    var day = document.getElementById("day").value;
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
+    var day = parseInt(document.getElementById("day").value, 10);
+    var month = parseInt(document.getElementById("month").value, 10);
+    var year = parseInt(document.getElementById("year").value, 10);
 
+    var usr_m = document.getElementById("username-m");
+    var pswd_m = document.getElementById("password-m");
+    var pswd_c_m = document.getElementById("password-confirm-m");
+    var date_m = document.getElementById("birth-date-m");
+
+    var birthDate = new Date(year, month - 1, day);
+    var curDate = new Date();
+    var minDate = new Date(curDate.getFullYear() - 15, curDate.getMonth(), curDate.getDate());
+    var maxDate = new Date(curDate.getFullYear() - 200, curDate.getMonth(), curDate.getDate());
+
+    var v = true;
+
+    if(username === '') {
+        usr_m.innerText = "Este campo es obligatorio. *";
+
+        v = false;
+    }
+    
+    if(password === '') {
+        pswd_m.innerText = "Este campo es obligatorio. *";
+        
+        v = false;
+    }
+
+    if(password_confirm === '') {
+        pswd_c_m.innerText = "Este campo es obligatorio. *";
+        
+        v = false;
+    } else if(password !== password_confirm) {
+        pswd_c_m.innerText = "Las contraseñas no coinciden. *";
+        
+        v = false;
+    }
+
+    if(!day || !month || !year) {
+        date_m.innerText = "Este campo es obligatorio. *";
+        
+        v = false;
+    } else if(birthDate > curDate) {
+        date_m.innerText = "No se admiten viajeros del futuro. *";
+        
+        v = false;
+    } else if(birthDate > minDate) {
+        date_m.innerText = "No se admiten menores a 15 años. *";
+        
+        v = false;
+    } else if(birthDate < maxDate) {
+        date_m.innerText = "No se admiten viajeros del pasado. *";
+        
+        v = false;
+    }
+
+    // validar si el usuario está en uso, si no, inicia sesión
+    
     console.log(username);
     console.log(password);
-    console.log(passwordConfirm);
+    console.log(password_confirm);
     console.log(`${day}/${month}/${year}`);
 
-    return false;
+    return v;
 }
